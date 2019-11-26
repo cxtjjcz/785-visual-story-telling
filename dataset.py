be13cee2e04c5d1bf1e17f0a96a9c01d84883ef6
Mike Chen <xiangtic@andrew.cmu.edu>
2019-11-25 23:54:26 -0500

    minor changes (added todo)

--- dataset.py
+++ dataset.py
@@ -10,6 +10,7 @@ import numpy as np
 import torch
 from hyperparams import *
 
+
 # Build dataset
 class StoryDataset(Dataset):
     def __init__(self, sis, vocab):
@@ -18,12 +19,11 @@ class StoryDataset(Dataset):
         self.vocab = vocab
         self.numpy_folder = './vist_api/images/Numpys/'
         self.pre_process()
-        
 
     def __len__(self):
-#         return 10
+        #         return 10
         return len(self.story_indices)
-    
+
     # Check if images have already been processed as numpy arrays, if not save 
     # them as numpy arrays
     def pre_process(self):
@@ -42,10 +42,10 @@ class StoryDataset(Dataset):
                 numpy_name = self.numpy_folder + story_id
                 to_save = np.array(imgs)
                 np.save(numpy_name, to_save)
-                
+
         end_time = time.time()
         print('Processing Images Time: ', end_time - start_time)
-    
+
     @staticmethod
     def read_image(path):
         img = Image.open(path)
@@ -62,22 +62,27 @@ class StoryDataset(Dataset):
         story = self.sis.Stories[story_id]
         my_imgs_path = self.numpy_folder + story_id + '.npy'
         imgs = torch.tensor(np.load(my_imgs_path))
-        
+
         ## Setence Stuff
         sent_ids = story['sent_ids']
         sent = ""
         for sent_id in sent_ids:
             # Add a space for the sentence, probably want to just remove puncuation
-            sent += " " + self.sis.Sents[sent_id]["text"] 
+            sent += " " + self.sis.Sents[sent_id]["text"]
         sent_tensor = self.vocab.sent2vec("<s> " + sent + " </s>")
-        
+
         # Return vals
         return imgs, sent_tensor
 
+
 def collate_story(seq_list):
+    """
+    TODO: change sents to be (batch_size, num_sents, max_sentence_length)
+          change sents_len to be (batch_size, num_sents, 1)
+    """
+
     imgs, sents = zip(*seq_list)
     imgs = torch.stack(imgs)
     sents_len = torch.Tensor([len(sent) for sent in sents])
     sents = pad_sequence(sents, padding_value=3)
     return imgs, sents, sents_len
-
