ee61f15badfea63c61ee54b169e538b0183646e2
Avenirrr <yy727498@hotmail.comwq>
2019-11-30 05:01:56 +0000

    changes made for model v2

--- run_model_v2.py
+++ run_model_v2.py
@@ -11,23 +11,20 @@ from vocab import Vocabulary
 from train_test import train, test
 
 vocab_save_path = "vocab.pt"
-vist_annotations_dir = '/Users/xiangtic/vist/'
-images_dir = '/Users/xiangtic/vist/images/'
-sis_train = Story_in_Sequence(images_dir + "toy", vist_annotations_dir)
-# sis_val = Story_in_Sequence(images_dir+"val", vist_annotations_dir)
+vist_annotations_dir = './vist_api/'
+images_dir = './vist_api/images/'
+sis_train = Story_in_Sequence(images_dir + "train", vist_annotations_dir)
+sis_val = Story_in_Sequence(images_dir+"val", vist_annotations_dir)
 # sis_test = Story_in_Sequence(images_dir+"test", vist_annotations_dir)
 
-cuda = False
-# cuda = cuda and torch.cuda.is_available()
-device = "cpu"
-
-if (not osp.exists(vocab_save_path)):
+if True:
+    print('Creating vocab')
     corpus = []
     for story in sis_train.Stories:
         sent_ids = sis_train.Stories[story]['sent_ids']
         for sent_id in sent_ids:
             corpus.append(sis_train.Sents[sent_id]['text'])
-    vocab = Vocabulary(corpus, freq_cutoff=1)  # reads and builds
+    vocab = Vocabulary(corpus, freq_cutoff=2)  # reads and builds
 
     # Verifying vocabulary is the same
     for word in vocab.w2i.keys():
@@ -37,13 +34,14 @@ if (not osp.exists(vocab_save_path)):
     # Saving vocabulary
     with open(vocab_save_path, 'wb') as file:
         pickle.dump(vocab, file)
+    print(len(vocab))
 else:
     vocab = pickle.load(open(vocab_save_path, 'rb'))
 
 
 def main():
     train_story_set = StoryDataset(sis_train, vocab)
-    # val_story_set = StoryDataset(sis_val, vocab)
+    val_story_set = StoryDataset(sis_val, vocab)
     # test_story_set = StoryDataset(sis_test, vocab)
 
     train_loader = DataLoader(train_story_set, shuffle=False, batch_size=BATCH_SIZE, collate_fn=collate_story,
@@ -52,20 +50,21 @@ def main():
     # sents BS * 5  * MAX_LEN
 
     model_v2 = ModelV2(vocab)
-    print(model_v2)
-
+    # print(model_v2)
+    model_v2.to(DEVICE)
     # Learning rate is the most sensitive value to set,
     # will need to test what works well past 400 instances
     optimizer = torch.optim.Adam(model_v2.parameters(), lr=0.001)  # .001 for 400
     isTraining = True
 
     if isTraining:
-        train(10, model_v1, train_loader, optimizer)
+        train(10, model_v2, train_loader, optimizer)
     else:
-        model_v1.load_state_dict(torch.load('./Training/7'))
-        test_loader = DataLoader(train_story_set, shuffle=False, batch_size=BATCH_SIZE, collate_fn=collate_story)
-        test(model_v1, test_loader, device, vocab)
+        model_v2.load_state_dict(torch.load('./Training/7'))
+        test_loader = DataLoader(val_story_set, shuffle=False, batch_size=BATCH_SIZE, collate_fn=collate_story)
+        test(model_v2, test_loader, vocab)
 
 
 if __name__ == "__main__":
     main()
+
